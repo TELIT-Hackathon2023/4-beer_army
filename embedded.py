81a60a5a56bcf7daaaa67c87a5aeb4659d427f75
Vladyslav Sakhnenko <vladyslav.sakhnenko.work@gmail.com>
2023-11-25 23:58:18 +0100

    init

--- embedded.py
+++ embedded.py
@@ -22,38 +22,23 @@ url = openai.api_base + "/openai/deployments?api-version=2022-12-01"
 
 r = requests.get(url, headers={"api-key": API_KEY})
 
-print(r.text)
-
-df=pd.read_csv(os.path.join(os.getcwd(),'bill_sum_data.csv')) # This assumes that you have placed the bill_sum_data.csv in the same directory you are running Jupyter Notebooks
-df_bills = df[['text', 'summary', 'title']]
+df=pd.read_csv(os.path.join(os.getcwd(),'Data-full-NEW.csv')) # This assumes that you have placed the bill_sum_data.csv in the same directory you are running Jupyter Notebooks
+print(df)
+df_bills = df['full_data']
 
 # print(df_bills)
 pd.options.mode.chained_assignment = None #https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#evaluation-order-matters
 
-# s is input text
-def normalize_text(s, sep_token = " \n "):
-    s = re.sub(r'\s+',  ' ', s).strip()
-    s = re.sub(r". ,","",s)
-    # remove all instances of multiple spaces
-    s = s.replace("..",".")
-    s = s.replace(". .",".")
-    s = s.replace("\n", "")
-    s = s.strip()
-    
-    return s
-
-df_bills['text']= df_bills["text"].apply(lambda x : normalize_text(x))
 
 tokenizer = tiktoken.get_encoding("cl100k_base")
-df_bills['n_tokens'] = df_bills["text"].apply(lambda x: len(tokenizer.encode(x)))
+df_bills['n_tokens'] = df_bills["full_data"].apply(lambda x: len(tokenizer.encode(x)))
 df_bills = df_bills[df_bills.n_tokens<8192]
 
 
-sample_encode = tokenizer.encode(df_bills.text[0]) 
+sample_encode = tokenizer.encode(df_bills.full_data[0]) 
 decode = tokenizer.decode_tokens_bytes(sample_encode)
-decode
 
-df_bills['ada_v2'] = df_bills["text"].apply(lambda x : get_embedding(x, engine = 'embeddings-GreenBox')) # engine should be set to the deployment name you chose when you deployed the text-embedding-ada-002 (Version 2) model
+df_bills['ada_v2'] = df_bills["full_data"].apply(lambda x : get_embedding(x, engine = 'embeddings-GreenBox')) # engine should be set to the deployment name you chose when you deployed the text-embedding-ada-002 (Version 2) model
 
 
 print(df_bills)
