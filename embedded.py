15fbfd606f5b259c53e7383c70368649b0b55cf1
Vladyslav Sakhnenko <vladyslav.sakhnenko.work@gmail.com>
2023-11-26 00:20:20 +0100

    add sllep() for reqwest time out

--- embedded.py
+++ embedded.py
@@ -7,6 +7,7 @@ from num2words import num2words
 import os
 import pandas as pd
 import numpy as np
+import time
 from openai.embeddings_utils import get_embedding, cosine_similarity
 import tiktoken
 
@@ -24,7 +25,7 @@ r = requests.get(url, headers={"api-key": API_KEY})
 
 df=pd.read_csv(os.path.join(os.getcwd(),'Data-full-NEW.csv')) # This assumes that you have placed the bill_sum_data.csv in the same directory you are running Jupyter Notebooks
 print(df)
-df_bills = df['full_data']
+df_bills = df[['full_data']]
 
 # print(df_bills)
 pd.options.mode.chained_assignment = None #https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#evaluation-order-matters
@@ -38,7 +39,12 @@ df_bills = df_bills[df_bills.n_tokens<8192]
 sample_encode = tokenizer.encode(df_bills.full_data[0]) 
 decode = tokenizer.decode_tokens_bytes(sample_encode)
 
-df_bills['ada_v2'] = df_bills["full_data"].apply(lambda x : get_embedding(x, engine = 'embeddings-GreenBox')) # engine should be set to the deployment name you chose when you deployed the text-embedding-ada-002 (Version 2) model
+def get_embedding_with_delay(text):
+    embedding = get_embedding(text, engine='embeddings-GreenBox')
+    time.sleep(1)  # Delay for 1 second
+    return embedding
+
+df_bills['ada_v2'] = df_bills["full_data"].apply(get_embedding_with_delay)# engine should be set to the deployment name you chose when you deployed the text-embedding-ada-002 (Version 2) model
 
 
 print(df_bills)
